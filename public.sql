bb7694ab2a6f0def8cacdf3ee60eca220a7a8a46
GYH <2350871838@qq.com>
2021-04-09 15:06:47 +0800

    添加数据库

--- public.sql
+++ public.sql
@@ -12,7 +12,7 @@
  Target Server Version : 130002
  File Encoding         : 65001
 
- Date: 07/04/2021 17:52:22
+ Date: 09/04/2021 15:05:02
 */
 
 
@@ -82,13 +82,23 @@ DROP TABLE IF EXISTS "public"."sc_room_message";
 CREATE TABLE "public"."sc_room_message" (
   "id" int4 NOT NULL DEFAULT nextval('sc_room_message_id_seq'::regclass),
   "room_id" int4 NOT NULL,
-  "useer_id" int4 NOT NULL,
-  "message" varchar(255) COLLATE "pg_catalog"."default"
+  "user_id" int4 NOT NULL,
+  "content" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "msg_type" int2 NOT NULL,
+  "path" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "name" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "avatar" varchar(255) COLLATE "pg_catalog"."default" NOT NULL,
+  "date" timestamp(6) NOT NULL DEFAULT now()
 )
 ;
 COMMENT ON COLUMN "public"."sc_room_message"."room_id" IS '房间id';
-COMMENT ON COLUMN "public"."sc_room_message"."useer_id" IS '用户id';
-COMMENT ON COLUMN "public"."sc_room_message"."message" IS '消息';
+COMMENT ON COLUMN "public"."sc_room_message"."user_id" IS '用户id';
+COMMENT ON COLUMN "public"."sc_room_message"."content" IS '消息';
+COMMENT ON COLUMN "public"."sc_room_message"."msg_type" IS '消息类型';
+COMMENT ON COLUMN "public"."sc_room_message"."path" IS '消息请求路径';
+COMMENT ON COLUMN "public"."sc_room_message"."name" IS '用户名';
+COMMENT ON COLUMN "public"."sc_room_message"."avatar" IS '用户头像';
+COMMENT ON COLUMN "public"."sc_room_message"."date" IS '时间';
 
 -- ----------------------------
 -- Table structure for sc_user
@@ -129,7 +139,7 @@ SELECT setval('"public"."sc_room_id_seq"', 36, true);
 -- ----------------------------
 ALTER SEQUENCE "public"."sc_room_message_id_seq"
 OWNED BY "public"."sc_room_message"."id";
-SELECT setval('"public"."sc_room_message_id_seq"', 2, false);
+SELECT setval('"public"."sc_room_message_id_seq"', 7, true);
 
 -- ----------------------------
 -- Alter sequences owned by
@@ -183,7 +193,7 @@ ALTER TABLE "public"."sc_user_room" ADD CONSTRAINT "sc_user_room_pkey" PRIMARY K
 -- Foreign Keys structure for table sc_room_message
 -- ----------------------------
 ALTER TABLE "public"."sc_room_message" ADD CONSTRAINT "sc_room_message_room_id_fkey" FOREIGN KEY ("room_id") REFERENCES "public"."sc_room" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
-ALTER TABLE "public"."sc_room_message" ADD CONSTRAINT "sc_room_message_useer_id_fkey" FOREIGN KEY ("useer_id") REFERENCES "public"."sc_user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "public"."sc_room_message" ADD CONSTRAINT "sc_room_message_useer_id_fkey" FOREIGN KEY ("user_id") REFERENCES "public"."sc_user" ("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- ----------------------------
 -- Foreign Keys structure for table sc_user_room
